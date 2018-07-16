0d63ce743e923442216f7076cf72d7e07b6abed6
jonathan <jonathan.medrano@benandfrank.com>
2018-07-16 11:56:53 -0500

    [REF] openacademy: Add inherit matter

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,12 +5,10 @@ from odoo import models, fields, api
 class Course(models.Model):
      _name = 'openacademy.course'
 
-     name = fields.Char(string ='Title', required=True)
+     name = fields.Char(string='Title', required=True)
      description = fields.Text()
-     responsible_id = fields.Many2one(
-          'res.users', string='Responsible',
-          index=True, ondelete='set null')
-     session_ids = fields.One2many('openacademy.session', 'course_id')
+     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
      _name = 'openacademy.session'
@@ -19,10 +17,12 @@ class Session(models.Model):
      start_date =fields.Date()
      duration = fields.Float(digits=(6, 2), help="Duration in days")
      seats = fields.Integer(string="Number of seats")
-     instructor_id = fields.Many2one('res.partner', string="Instructor")
-     course_id = fields.Many2one('openacademy.course', ondelete="cascade",
-                                 string="Course", required=True)
+     instructor_id = fields.Many2one('res.partner', string="Instructor" ,
+                                     domain=['|' , ('instructor' ,  '=' , True), ('category_id.name' , 'ilke' , 'Teacher')])
+
+     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
      attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
 
 
 
