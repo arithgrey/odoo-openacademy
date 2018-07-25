2fce452acf7f4b078e873e2bc0c2eb4b03487f36
jonathan <jonathan.medrano@benandfrank.com>
2018-07-25 15:58:54 -0500

    [REF] refactoring especificaciones travis

--- openacademy/wizard.py
+++ openacademy/wizard.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields
+
 
 class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
 
     def _default_session(self):
-
         session_obj = self.env['openacademy.session']
         session_id = self._context.get('active_id')
-        session_record =  session_obj.browse(session_id)
-        #import pdb; pdb.set_trace()
+        session_record = session_obj.browse(session_id)
+        # import pdb; pdb.set_trace()
         return session_record
 
     session_id = fields.Many2one('openacademy.session',
