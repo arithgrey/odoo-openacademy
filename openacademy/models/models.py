cb6b38166112f9c38198e5c8f8ed84958a1b14c8
jonathan <jonathan.medrano@benandfrank.com>
2018-07-25 16:06:37 -0500

    [REF] refactoring especificaciones travis

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -20,6 +20,7 @@ class Course(models.Model):
         ('name_description_check',
          'CHECK(name != description)',
          "The title of the course should not be the description"),
+
         ('name_unique',
          'UNIQUE(name)',
          "The course title must be unique"),
